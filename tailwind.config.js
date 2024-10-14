285e106859a4f25393ae54faefa828adbfcd3e73
Isanchez-LewisU <ivansanchez@lewisu.edu>
2024-10-13 21:25:17 -0500

    Updated the main page styling using tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,11 +5,10 @@ export const content = [
 export const theme = {
   extend: {
     colors: {
-      primary: "#a6192e", // Maroon
-      secondary: "#8a1c1e", // Dark Red
       white: "#ffffff",
       gray: "#d9d9d9",
       black: "#000000",
+      lewisRed: "#940731",  // Same color as the MyLewis portal
     },
   },
 };
