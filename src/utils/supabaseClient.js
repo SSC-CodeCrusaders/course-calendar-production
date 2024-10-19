b3cd7d54e4f195903764ede1ec1b51ad89946b88
Mateusz Obrochta <mateusz091500@gmail.com>
2024-10-19 17:10:52 -0500

    Added .env config

--- src/utils/supabaseClient.js
+++ src/utils/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://otgflatuxludmkdetdbc.supabase.co';
-const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im90Z2ZsYXR1eGx1ZG1rZGV0ZGJjIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjkxMzk3MDgsImV4cCI6MjA0NDcxNTcwOH0.jI1LE1yIv-8t3gQzdce8IlL9Won1xIxhjTsJx-eFqcU';
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY;
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
