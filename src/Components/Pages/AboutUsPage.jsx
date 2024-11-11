3682e0042ab543c32ea66953130e6b2fe5dadefd
Mateusz Obrochta <mateusz091500@gmail.com>
2024-11-11 13:03:58 -0600

    Fixed image issue

--- src/Components/Pages/AboutUsPage.jsx
+++ src/Components/Pages/AboutUsPage.jsx
@@ -7,7 +7,7 @@ const AboutUsPage = () => {
       <div className="p-3 bg-lewisRed text-white text-center">Meet the Team Behind the Project</div>
 
       <div class="flex items-center">
-        <img scr="./img/matt.jpg" alt='Matthew Bilinski' className="w-32 h-32 rounded-full mb-4 object-cover"/>
+        <img src="/public/img/matt.jpg" alt='Matthew Bilinski' className="w-32 h-32 rounded-full mb-4 object-cover"/>
         <div>
           <strong>Matthew Bilinski</strong>
           <br></br>
