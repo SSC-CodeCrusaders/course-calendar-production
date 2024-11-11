17a6254129c62c0bea5946eb25a234963a4a24a4
Matthew Shouse <mshouse8624@gmail.com>
2024-11-11 12:55:55 -0600

    Added Matt B. Image, img folder under public, and starting working on About Us Page. - Shouse

--- src/Components/Pages/AboutUsPage.jsx
+++ src/Components/Pages/AboutUsPage.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
 
 const AboutUsPage = () => {
-  return <div className="p-4 bg-green-900 text-white">AboutUsPage</div>;
+  return (
+    <>
+      <div className="p-1 bg-lewisRed text-white text-center font-bold">About Us</div>
+      <div className="p-3 bg-lewisRed text-white text-center">Meet the Team Behind the Project</div>
+
+      <div class="flex items-center">
+        <img scr="./img/matt.jpg" alt='Matthew Bilinski' className="w-32 h-32 rounded-full mb-4 object-cover"/>
+        <div>
+          <strong>Matthew Bilinski</strong>
+          <br></br>
+          <a href="mailto:matthewrbilinski@lewisu.edu">matthewrbilinski@lewisu.edu</a>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default AboutUsPage;
