f96dfe665c39b481c36f84726dd6a8f3ae25ba6e
jayes5089 <jayes5089@gmail.com>
2025-04-13 17:29:48 -0500

    fixing route 404 attempt

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import path from 'path';
 import { fileURLToPath } from 'url';
+
 const app = express();
 const port = process.env.PORT || 3000;
 
@@ -8,11 +9,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 app.use(express.static(path.join(__dirname, 'dist')));
-
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
-
 app.listen(port, () => {
-    console.log(`Server running on port ${port}`);
+    console.log(`Server running on port $${port}`);
 });
