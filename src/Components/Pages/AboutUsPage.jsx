ee6bdee6d62fedcb001eedeea60cdd4ff696776c
Aaron-2792 <aaron92727@hotmail.com>
2025-04-19 16:09:37 -0500

    added Aaron in about us section

--- src/Components/Pages/AboutUsPage.jsx
+++ src/Components/Pages/AboutUsPage.jsx
@@ -97,6 +97,16 @@ const AboutUsPage = () => {
             </div>
           </div>
 
+          {/*Aaron's About*/}
+          <div className='flex items-center group/item hover:bg-red-700 bg-lewisRed p-2'>
+            <img src="/img/aaron.jpg" alt='Aaron Rader' className=" ml-2 mr-1 mt-2 w-32 h-32 rounded-full object-cover"/>
+            <div className='ml-2 overflow-hidden'>
+              <strong className='mb-1 text-xl text-white'>Benyamin Bamburac</strong>
+              <br></br>
+              <a className="text-b  truncate text-white" href="mailto:aaronrader@lewisu.edu">aaronrader@lewisu.edu</a>
+            </div>
+          </div>
+        
           {/*Benyamin's About*/}
           <div className='flex items-center group/item hover:bg-red-700 bg-lewisRed p-2'>
             <img src="/img/benyamin.jpg" alt='Benyamin Bamburac' className=" ml-2 mr-1 mt-2 w-32 h-32 rounded-full object-cover"/>
@@ -108,6 +118,7 @@ const AboutUsPage = () => {
           </div>
         </div>
       </div>
+      
 
       {/*
       <div className="w-screen h-screen">
